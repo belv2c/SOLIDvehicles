a4374162c02e48e0d64a6784e777872e9584be24
Brooke Lewis <belv2c@gmail.com>
2018-02-02 18:31:52 -0600

    setup all files, created interface and classes

--- SOLIDvehicles/Program.cs
+++ SOLIDvehicles/Program.cs
@@ -10,6 +10,16 @@ namespace SOLIDvehicles
     {
         static void Main(string[] args)
         {
+            // Build a collection of all vehicles that fly
+            // With a single `foreach`, have each vehicle Fly()
+
+
+            // Build a collection of all vehicles that operate on roads
+            // With a single `foreach`, have each road vehicle Drive()
+
+
+            // Build a collection of all vehicles that operate on water
+            // With a single `foreach`, have each water vehicle Drive()
         }
     }
 }
---
+++ SOLIDvehicles/Vehicles/Yacht.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLIDvehicles.Vehicles
+{
+    public class Yacht : IVehicle
+    {
+        public int Wheels { get; set; } = 3;
+        public int Doors { get; set; } = 3;
+        public int PassengerCapacity { get; set; } = 113;
+        public bool Winged { get; set; } = true;
+        public string TransmissionType { get; set; } = "None";
+        public double EngineVolume { get; set; } = 31.1;
+        public double MaxWaterSpeed { get; set; }
+        public double MaxLandSpeed { get; set; }
+        public double MaxAirSpeed { get; set; } = 309.0;
+
+        public void Drive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine("");
+        }
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
